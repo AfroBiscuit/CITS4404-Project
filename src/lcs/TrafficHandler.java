c0c1d572714a5c477ed75aedb657df430e20f2f4
AfroBiscuit <cockman2@gmail.com>
2014-10-08 21:45:32 +0800

    Added skeleton to traffic handling class

---
+++ src/lcs/TrafficHandler.java
@@ -0,0 +1,43 @@
+package lcs;
+
+public class TrafficHandler {
+	
+	public int laneNorth;
+	public int laneSouth;
+	public int laneEast;
+	public int laneWest;
+	
+	public void trafficLoop(){
+		//implement main algorithm here
+	}
+	
+	public void highTraffic(int lane){
+		//implement high traffic switcher here
+	}
+	
+	public void medTraffic(int lane){
+		//implement medium traffic switcher here
+	}
+	
+	public void lowTraffic(int lane){
+		//implement low traffic switcher here
+	}
+	
+	public int getNorth(){
+		return laneNorth;
+	}
+	
+	public int getNSouth(){
+		return laneSouth;
+	}
+	
+	public int getEast(){
+		return laneEast;
+	}
+	
+	public int getWest(){
+		return laneWest;
+	}
+	
+}
+
