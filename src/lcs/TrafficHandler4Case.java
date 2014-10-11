805829fc2cd2ce5182032d21b43e7b04906f1f62
AfroBiscuit <cockman2@gmail.com>
2014-10-11 22:01:51 +0800

    Implemented turning lanes - UNTESTED
    Still need to test whole system, however it does compile

--- src/lcs/TrafficHandler4Case.java
+++ src/lcs/TrafficHandler4Case.java
@@ -11,11 +11,6 @@ import java.util.Random;
 public class TrafficHandler4Case {
 	public TrafficHandler4Case(){}
 	
-	public TrafficHandler traffic1 = new TrafficHandler(); //top left
-	public TrafficHandler traffic2 = new TrafficHandler(); //top right
-	public TrafficHandler traffic3 = new TrafficHandler(); //bottom left
-	public TrafficHandler traffic4 = new TrafficHandler(); //bottom right
-	
 	LaneIncrement1 lInc1 = new LaneIncrement1();
 	LaneIncrement2 lInc2 = new LaneIncrement2();
 	LaneIncrement3 lInc3 = new LaneIncrement3();
@@ -58,16 +53,30 @@ public class TrafficHandler4Case {
 		//S&E for traffic4
 	//all other sources of traffic at intersections come from other intersections
 	//cars only exit the system at these same points
-	public void setup(){
-		traffic1.ignore(1, 2);
-		traffic2.ignore(1, 3);
-		traffic3.ignore(0, 2);
-		traffic4.ignore(0, 3);
-	}
-	
+
 	public void runTraffic(){
 		
 		startTime = System.currentTimeMillis();
+		
+		lInc1.start();
+		lInc2.start();
+		lInc3.start();
+		lInc4.start();
+		
+		lDec1.start();
+		lDec2.start();
+		lDec3.start();
+		lDec4.start();
+		
+		if(!runningInc1) lInc1.interrupt();
+		if(!runningInc2) lInc2.interrupt();
+		if(!runningInc3) lInc3.interrupt();
+		if(!runningInc4) lInc4.interrupt();
+		
+		if(!runningDec1) lDec1.interrupt();
+		if(!runningDec2) lDec2.interrupt();
+		if(!runningDec3) lDec3.interrupt();
+		if(!runningDec4) lDec4.interrupt();
 		
 		
 	}
@@ -84,6 +93,21 @@ public class TrafficHandler4Case {
 		prob = 50;
 	}
 	
+	public int getLaneNumbers(int light, int lane){
+		int position = light*4 + lane;
+		return laneNumbers[position];
+	}
+	
+	public void changeState(int light, int state){
+		if(light == 0) state1 = state;
+		else if(light == 1) state2 = state;
+		else if (light == 2) state3 = state;
+		else if (light ==3) state4 = state;
+		else{
+			//chuck an error
+		}
+	}
+	//PRIVATE CLASSES BELOW FOR EACH TRAFFIC LIGHT
 	private class LaneIncrement1 extends Thread{
 		//variables
 		int[] ignoreLanes = {1,2};
@@ -229,8 +253,14 @@ public class TrafficHandler4Case {
 					//North & South turn right, East & West turn left
 					while(System.currentTimeMillis() - loopStart < laneDecSleep){
 						if(laneNumbers[0]>0) laneNumbers[0]--; //North Lane
-						if(laneNumbers[1]>0) laneNumbers[1]--; //South Lane
-						if(laneNumbers[2]>0) laneNumbers[2]--; //East Lane
+						if(laneNumbers[1]>0){
+							laneNumbers[1]--; //South Lane
+							laneNumbers[7]++;
+						}
+						if(laneNumbers[2]>0){
+							laneNumbers[2]--; //East Lane
+							laneNumbers[8]++;
+						}
 						if(laneNumbers[3]>0) laneNumbers[3]--; //West Lane
 						try {
 							Thread.sleep(500);
@@ -243,10 +273,16 @@ public class TrafficHandler4Case {
 				else if (state1 == 3){
 					//East & West turn right, North & South turn left
 					while(System.currentTimeMillis() - loopStart < laneDecSleep){
-						if(laneNumbers[0]>0) laneNumbers[0]--; //North Lane
+						if(laneNumbers[0]>0){
+							laneNumbers[0]--; //North Lane
+							laneNumbers[7]++;
+						}
 						if(laneNumbers[1]>0) laneNumbers[1]--; //South Lane
 						if(laneNumbers[2]>0) laneNumbers[2]--; //East Lane
-						if(laneNumbers[3]>0) laneNumbers[3]--; //West Lane
+						if(laneNumbers[3]>0){
+							laneNumbers[3]--; //West Lane
+							laneNumbers[8]++;
+						}
 						try {
 							Thread.sleep(500);
 						} catch (InterruptedException e) {
@@ -404,9 +440,15 @@ public class TrafficHandler4Case {
 				else if (state2 == 2){
 					//North & South turn right, East & West turn left
 					while(System.currentTimeMillis() - loopStart < laneDecSleep){
-						if(laneNumbers[4]>0) laneNumbers[4]--; //North Lane
+						if(laneNumbers[4]>0){
+							laneNumbers[4]--; //North Lane
+							laneNumbers[2]++;
+						}
 						if(laneNumbers[5]>0) laneNumbers[5]--; //South Lane
-						if(laneNumbers[6]>0) laneNumbers[6]--; //East Lane
+						if(laneNumbers[6]>0){
+							laneNumbers[6]--; //East Lane
+							laneNumbers[12]++;
+						}
 						if(laneNumbers[7]>0) laneNumbers[7]--; //West Lane
 						try {
 							Thread.sleep(500);
@@ -420,9 +462,15 @@ public class TrafficHandler4Case {
 					//East & West turn right, North & South turn left
 					while(System.currentTimeMillis() - loopStart < laneDecSleep){
 						if(laneNumbers[4]>0) laneNumbers[4]--; //North Lane
-						if(laneNumbers[5]>0) laneNumbers[5]--; //South Lane
+						if(laneNumbers[5]>0){
+							laneNumbers[5]--; //South Lane
+							laneNumbers[2]++;
+						}
 						if(laneNumbers[6]>0) laneNumbers[6]--; //East Lane
-						if(laneNumbers[7]>0) laneNumbers[7]--; //West Lane
+						if(laneNumbers[7]>0){
+							laneNumbers[7]--; //West Lane
+							laneNumbers[12]++;
+						}
 						try {
 							Thread.sleep(500);
 						} catch (InterruptedException e) {
@@ -581,9 +629,15 @@ public class TrafficHandler4Case {
 					//North & South turn right, East & West turn left
 					while(System.currentTimeMillis() - loopStart < laneDecSleep){
 						if(laneNumbers[8]>0) laneNumbers[8]--; //North Lane
-						if(laneNumbers[9]>0) laneNumbers[9]--; //South Lane
+						if(laneNumbers[9]>0){
+							laneNumbers[9]--; //South Lane
+							laneNumbers[15]++;
+						}
 						if(laneNumbers[10]>0) laneNumbers[10]--; //East Lane
-						if(laneNumbers[11]>0) laneNumbers[11]--; //West Lane
+						if(laneNumbers[11]>0){
+							laneNumbers[11]--; //West Lane
+							laneNumbers[1]++;
+						}
 						try {
 							Thread.sleep(500);
 						} catch (InterruptedException e) {
@@ -595,9 +649,15 @@ public class TrafficHandler4Case {
 				else if (state3 == 3){
 					//East & West turn right, North & South turn left
 					while(System.currentTimeMillis() - loopStart < laneDecSleep){
-						if(laneNumbers[8]>0) laneNumbers[8]--; //North Lane
+						if(laneNumbers[8]>0){
+							laneNumbers[8]--; //North Lane
+							laneNumbers[15]++;
+						}
 						if(laneNumbers[9]>0) laneNumbers[9]--; //South Lane
-						if(laneNumbers[10]>0) laneNumbers[10]--; //East Lane
+						if(laneNumbers[10]>0){
+							laneNumbers[10]--; //East Lane
+							laneNumbers[1]++;
+						}
 						if(laneNumbers[11]>0) laneNumbers[11]--; //West Lane
 						try {
 							Thread.sleep(500);
@@ -756,10 +816,16 @@ public class TrafficHandler4Case {
 				else if (state4 == 2){
 					//North & South turn right, East & West turn left
 					while(System.currentTimeMillis() - loopStart < laneDecSleep){
-						if(laneNumbers[12]>0) laneNumbers[12]--; //North Lane
+						if(laneNumbers[12]>0){
+							laneNumbers[12]--; //North Lane
+							laneNumbers[10]++;
+						}
 						if(laneNumbers[13]>0) laneNumbers[13]--; //South Lane
 						if(laneNumbers[14]>0) laneNumbers[14]--; //East Lane
-						if(laneNumbers[15]>0) laneNumbers[15]--; //West Lane
+						if(laneNumbers[15]>0){
+							laneNumbers[15]--; //West Lane
+							laneNumbers[5]++;
+						}
 						try {
 							Thread.sleep(500);
 						} catch (InterruptedException e) {
@@ -772,8 +838,14 @@ public class TrafficHandler4Case {
 					//East & West turn right, North & South turn left
 					while(System.currentTimeMillis() - loopStart < laneDecSleep){
 						if(laneNumbers[12]>0) laneNumbers[12]--; //North Lane
-						if(laneNumbers[13]>0) laneNumbers[13]--; //South Lane
-						if(laneNumbers[14]>0) laneNumbers[14]--; //East Lane
+						if(laneNumbers[13]>0){
+							laneNumbers[13]--; //South Lane
+							laneNumbers[10]++;
+						}
+						if(laneNumbers[14]>0){
+							laneNumbers[14]--; //East Lane
+							laneNumbers[5]++;
+						}
 						if(laneNumbers[15]>0) laneNumbers[15]--; //West Lane
 						try {
 							Thread.sleep(500);
